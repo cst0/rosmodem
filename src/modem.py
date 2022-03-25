ead650b1b6f31ea5e7133a68a83df248deba605c
Chris Thierauf <christopher.thierauf@tufts.edu>
2022-03-25 18:52:38 -0400

    dynamic sub/pub creation, more todo

--- src/modem.py
+++ src/modem.py
@@ -5,14 +5,48 @@ import rospy
 from abc import ABCMeta, abstractmethod
 
 
-class PublishWrapper:
-    def __init__(self, topic_type: str):
-        split = topic_type.strip("\n\r\t ;").split(",")
+class InterfaceWrapper:
+    def populate_from_split(self, topic_type_pair):
+        split = topic_type_pair.strip("\n\r\t ;").split(",")
         self.topic = split[0]
         _type = split[1]
 
         self.mtype = get_message_class(_type)
+
+
+class PublisherWrapper(InterfaceWrapper):
+    def __init__(self, topic_type: str):
+        self.populate_from_split(topic_type)
         self.pub = rospy.Publisher(self.topic, self.mtype, queue_size=1)
+
+    def shutdown(self):
+        self.pub.unregister()
+
+
+class SubscriberWrapper(InterfaceWrapper):
+    def __init__(self, topic_type: str):
+        self.populate_from_split(topic_type)
+        self.sub = rospy.Subscriber(self.topic, self.mtype, self._cb, queue_size=1)
+        self._has_message = False
+
+    def shutdown(self):
+        self.sub.unregister()
+
+    def _cb(self, msg):
+        self._has_message = True
+        self.msg = msg
+
+    def _get_message(self):
+        if self._has_message:
+            self._has_message = False
+            return self.msg
+        return None
+
+    def to_sendable_str(self):
+        returnme = ""
+        gm = self._get_message()
+        if gm:
+            returnme += "D[" + self.topic + ";" + gm + "]"
 
 
 class Modem(metaclass=ABCMeta):
@@ -71,25 +105,67 @@ class Modem(metaclass=ABCMeta):
     def place_in_write_queue(self, b):
         if type(b) is str:
             b = b.encode()
-        self.write_queue.append(b)
+        self._write_queue.append(b)
 
     def _put_msg_in_queue(self, msg):
         self._message_queue.append(msg)
 
-    def _manage_informed_topics(self):
-        unpublished_topics_types = set(self._informed_topics) - set(
-            self._publishers.keys()
+    def _manage_informed_subscribers(self):
+        """
+        The other end has told us what they want. Let's set up a subscriber
+        and send that data over (or remove a subscriber if nobody is using it anymore)
+        """
+        unsubscribed_topic_type_pairs = set(
+            self._informed_subscriber_topic_type_pairs
+        ) - set(self._subscribers.keys())
+        subscribed_topic_type_pairs_dead = set(self._subscribers.keys()) - set(
+            self._informed_subscriber_topic_type_pairs
         )
-        published_topics_types_dead = set(self._publishers.keys()) - set(
-            self._informed_topics
-        )
-        for topic_type in unpublished_topics_types:
-            self._publishers[topic_type] = PublishWrapper(topic_type)
-        for topic_type in published_topics_types_dead:
-            self._publishers[topic_type].shutdown()
-            del self._publishers[topic_type]
 
-    def _inform_topics(self):
+        for topic_type_pair in unsubscribed_topic_type_pairs:
+            self._subscribers[topic_type_pair] = SubscriberWrapper(topic_type_pair)
+
+        for topic_type_pair in subscribed_topic_type_pairs_dead:
+            self._subscribers[topic_type_pair].shutdown()
+            del self._subscribers[topic_type_pair]
+
+    def _inform_subscribers(self):
+        """
+        The other end has publishers that correspond with the publishers we're
+        actively getting data from here. If anyone subscribes to one of those
+        publishers, we need to ask the other side to reach out and actually
+        query that data.
+        """
+        in_use = []
+        for p in self._publishers:
+            if p.has_subscribers():
+                in_use.append(p.topic)
+        topiclist = "".join(["" + str(t) + ";" for t in in_use])
+        self.place_in_write_queue("S[" + topiclist + "]S")
+
+    def _manage_informed_publishers(self):
+        """
+        The other end of the modem has told us what they have available.
+        Let's make those topics available on our end, too.
+        """
+        unpublished_topic_type_pairs = set(
+            self._informed_publisher_topic_type_pairs
+        ) - set(self._publishers.keys())
+        published_topic_type_pairs_dead = set(self._publishers.keys()) - set(
+            self._informed_publisher_topic_type_pairs
+        )
+
+        for topic_type_pair in unpublished_topic_type_pairs:
+            self._publishers[topic_type_pair] = PublisherWrapper(topic_type_pair)
+
+        for topic_type_pair in published_topic_type_pairs_dead:
+            self._publishers[topic_type_pair].shutdown()
+            del self._publishers[topic_type_pair]
+
+    def _inform_publishers(self):
+        """
+        Inform the other end of the modem what topics we have available on our end.
+        """
         t = rospy.get_published_topics()
         found_topics = [x[0] for x in t]
         found_types = [x[1] for x in t]
@@ -104,11 +180,12 @@ class Modem(metaclass=ABCMeta):
 
     def __init__(self, args):
         self.max_read_len = 1024
-        self.write_queue = []
 
+        self._write_queue = []
         self._read_bytes = []
         self._message_queue = []
-        self._informed_topics = []
+        self._informed_publisher_topic_type_pairs = []
+        self._informed_subscriber_topic_type_pairs = []
 
         self._publishers = {}
         self._subscribers = {}
