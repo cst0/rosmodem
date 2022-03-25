a6acb31ae2f6783cc82748bd1b1809d5dc467a5a
Chris Thierauf <christopher.thierauf@tufts.edu>
2022-03-25 19:55:44 -0400

    read queue, thread safety, moving a few functions

--- src/modem.py
+++ src/modem.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-from genpy.message import get_message_class
 import rospy
+import re
+import threading
+from genpy.message import get_message_class
 from abc import ABCMeta, abstractmethod
+from typing import Union
 
 
 class InterfaceWrapper:
@@ -10,8 +13,9 @@ class InterfaceWrapper:
         split = topic_type_pair.strip("\n\r\t ;").split(",")
         self.topic = split[0]
         _type = split[1]
-
         self.mtype = get_message_class(_type)
+
+        self._content_lock = threading.Lock()
 
 
 class PublisherWrapper(InterfaceWrapper):
@@ -33,20 +37,23 @@ class SubscriberWrapper(InterfaceWrapper):
         self.sub.unregister()
 
     def _cb(self, msg):
-        self._has_message = True
-        self.msg = msg
+        with self._content_lock:
+            self._has_message = True
+            self.msg = msg
 
     def _get_message(self):
-        if self._has_message:
-            self._has_message = False
-            return self.msg
-        return None
+        with self._content_lock:
+            if self._has_message:
+                self._has_message = False
+                return self.msg
+            return None
 
     def to_sendable_str(self):
-        returnme = ""
-        gm = self._get_message()
-        if gm:
-            returnme += "D[" + self.topic + ";" + gm + "]"
+        with self._content_lock:
+            returnme = ""
+            gm = self._get_message()
+            if gm:
+                returnme += "D[" + self.topic + ";" + gm + "]"
 
 
 class Modem(metaclass=ABCMeta):
@@ -88,27 +95,80 @@ class Modem(metaclass=ABCMeta):
         """
         pass
 
+    def place_in_write_queue(self, s: str):
+        with self._write_lock:
+            s = s.replace("[", "\\[").replace("]", "\\]").replace("\\", "\\\\")
+            b = s.encode("UTF-8")
+            self._write_queue.append(b)
+
+    def _handle_subscribe_commands(self, s_segments):
+        pass
+
+    def _handle_publish_commands(self, p_segments):
+        pass
+
+    def _handle_data_commands(self, d_segments):
+        pass
+
     def _call_reads(self, event):
         del event
-        read = self.read(self.max_read_len)
-        if len(read) > 0:
-            self._read_bytes.append(read)
+        with self._read_lock:
+            read = self.read(self.max_read_len)
+            if len(read) > 0:
+                self._read_bytes.append(read)
+
+    def _handle_read_queue(self, event):
+        del event
+        with self._read_lock:
+            appended_bytes = b""
+            for b in self._read_bytes:
+                appended_bytes += b
+            s = appended_bytes.decode("UTF-8")
+            s = self._last_leftovers + s
+            self._last_leftovers = ""
+
+            # TODO: refactor, there's a bunch of reused code here
+            s_starts = [i.start() for i in re.finditer("S\\[", s)]
+            s_ends = [i.start() for i in re.finditer("\\]S", s)]
+            s_segments = []
+            for n in range(len(s_starts)):
+                s_segments.append(s[s_starts[n] + len("S[") : s_ends[n]])
+            if s_starts != s_ends:
+                self._last_leftovers = s[s_starts[-1] + len("S[") :]
+
+            p_starts = [i.start() for i in re.finditer("P\\[", s)]
+            p_ends = [i.start() for i in re.finditer("\\]P", s)]
+            p_segments = []
+            for n in range(len(p_starts)):
+                p_segments.append(s[p_starts[n] + len("P[") : p_ends[n]])
+            if p_starts != p_ends:
+                self._last_leftovers = s[p_starts[-1] + len("P[") :]
+
+            d_starts = [i.start() for i in re.finditer("D\\[", s)]
+            d_ends = [i.start() for i in re.finditer("\\]D", s)]
+            d_segments = []
+            for n in range(len(d_starts)):
+                d_segments.append(s[d_starts[n] + len("D[") : d_ends[n]])
+            if d_starts != d_ends:
+                self._last_leftovers = s[d_starts[-1] + len("D[") :]
+
+            self._handle_subscribe_commands(s_segments)
+            self._handle_publish_commands(p_segments)
+            self._handle_data_commands(d_segments)
+
+            # s = s.replace('\\[', '[').replace('\\]', ']').replace('\\\\','\\')
 
     def _handle_write_queue(self, event):
         del event
-        pass
+        with self._write_lock:
+            packet = ""
+            for w in self._write_queue:
+                packet += w
 
-    def _process_queue(self, event):
-        del event
-        pass
+            if packet:
+                self.write(packet)
 
-    def place_in_write_queue(self, b):
-        if type(b) is str:
-            b = b.encode()
-        self._write_queue.append(b)
-
-    def _put_msg_in_queue(self, msg):
-        self._message_queue.append(msg)
+            self._write_queue = []
 
     def _manage_informed_subscribers(self):
         """
@@ -129,20 +189,6 @@ class Modem(metaclass=ABCMeta):
             self._subscribers[topic_type_pair].shutdown()
             del self._subscribers[topic_type_pair]
 
-    def _inform_subscribers(self):
-        """
-        The other end has publishers that correspond with the publishers we're
-        actively getting data from here. If anyone subscribes to one of those
-        publishers, we need to ask the other side to reach out and actually
-        query that data.
-        """
-        in_use = []
-        for p in self._publishers:
-            if p.has_subscribers():
-                in_use.append(p.topic)
-        topiclist = "".join(["" + str(t) + ";" for t in in_use])
-        self.place_in_write_queue("S[" + topiclist + "]S")
-
     def _manage_informed_publishers(self):
         """
         The other end of the modem has told us what they have available.
@@ -161,6 +207,20 @@ class Modem(metaclass=ABCMeta):
         for topic_type_pair in published_topic_type_pairs_dead:
             self._publishers[topic_type_pair].shutdown()
             del self._publishers[topic_type_pair]
+
+    def _inform_subscribers(self):
+        """
+        The other end has publishers that correspond with the publishers we're
+        actively getting data from here. If anyone subscribes to one of those
+        publishers, we need to ask the other side to reach out and actually
+        query that data.
+        """
+        in_use = []
+        for p in self._publishers:
+            if p.has_subscribers():
+                in_use.append(p.topic)
+        topiclist = "".join(["" + str(t) + ";" for t in in_use])
+        self.place_in_write_queue("S[" + topiclist + "]S")
 
     def _inform_publishers(self):
         """
@@ -186,9 +246,13 @@ class Modem(metaclass=ABCMeta):
         self._message_queue = []
         self._informed_publisher_topic_type_pairs = []
         self._informed_subscriber_topic_type_pairs = []
+        self._last_leftovers = ""
 
         self._publishers = {}
         self._subscribers = {}
+
+        self._write_lock = threading.Lock()
+        self._read_lock = threading.Lock()
 
         self.initialize(args)
         rospy.on_shutdown(self.deinitialize)
